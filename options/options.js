9361d8d4b25d685105d596a3f09965cb3f833169
Crowdbotics <team@crowdbotics.com>
2021-08-25 22:58:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dry_resonance_30112Identifier",
+  url: "https://dry_resonance_30112Identifier.botics.co",
+  api: "https://dry_resonance_30112Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
